ed8fe5882dd37f0711eb8adb5daa73984c0c26b6
Ahmed_Bahgat <ahbahgat01@gmail.com>
2023-01-26 06:46:26 +0200

    refactored BombController

--- modules/BombController.js
+++ modules/BombController.js
@@ -2,61 +2,64 @@ import { ConfigController } from './ConfigController.js';
 
 export class BombController {
   #htmlTemplate = document.querySelector('img.bomb-template');
-  #explosionRadius;
   #level;
-  constructor(level) {
+  constructor(level = 1) {
     console.log('Bomb constructor');
-    this.#explosionRadius = 500; //TODO: take from configs only
     this.#level = level;
-    // this.bombConfigs = new ConfigController(1).configs.bombConfigs;
   }
 
   throwBomb() {
-    const bombConfigs = new ConfigController().configs.bombConfigs;
-    // console.log(this.bombConfigs);
+    const bombConfigs = new ConfigController(this.#level).configs.bombConfigs;
     let newBomb = this.#htmlTemplate.cloneNode();
     // TODO: ref
     newBomb.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
     document.body.append(newBomb);
 
     // add click listener for new bomb
-    newBomb.addEventListener('click', () => {
-      console.log('bomb clicked');
-      let eventData = {
-        bombCenter: {
-          x: parseInt(newBomb.style.left) + 0.5 * newBomb.width,
-          y: parseInt(newBomb.style.top) + 0.5 * newBomb.height,
-        },
-        explodeRadius: this.#explosionRadius, //RADIUS
-      };
-      // dispatchEvent on explosion
-      console.log('event data on bomb');
-      console.log(eventData);
-
-      const explodeevent = new CustomEvent('bombexplode', {
-        detail: eventData,
-        bubbles: true,
-      });
-      newBomb.dispatchEvent(explodeevent);
+    newBomb.addEventListener('click', function () {
+      bombClickListener(this, bombConfigs.explosionRadius);
     });
-
     // move bomb
-    let topPosition = 0 - newBomb.height;
-    newBomb.style.top = `${topPosition}px`;
-    newBomb.classList.remove('hidden');
-    let thownBombIntervalID = setInterval(function () {
-      console.log(`pos: ${newBomb.style.top}`);
-      newBomb.style.top = `${topPosition}px`;
-      // console.log(`bombstep: ${bombConfigs.bombMovingStep}`);
-      // console.log(`bombinterval: ${bombConfigs.bombMovingInterval}`);
-
-      topPosition += bombConfigs.bombMovingStep; // TODO: put steps into configuration files
-      if (topPosition > window.innerHeight) {
-        console.log('bomb will be removed');
-        clearInterval(thownBombIntervalID);
-        newBomb.remove();
-      }
-    }, bombConfigs.bombMovingInterval);
-    console.log('throw bomb()');
+    moveBomb(
+      newBomb,
+      bombConfigs.bombMovingStep,
+      bombConfigs.bombMovingInterval
+    );
   }
 }
+
+// === Utility Functions ===
+
+let bombClickListener = function (newBomb, explosionRadius) {
+  console.log('bomb clicked');
+  let eventData = {
+    bombCenter: {
+      x: parseInt(newBomb.style.left) + 0.5 * newBomb.width,
+      y: parseInt(newBomb.style.top) + 0.5 * newBomb.height,
+    },
+    explodeRadius: explosionRadius,
+  };
+  // dispatch event on explosion
+  const explodeevent = new CustomEvent('bombexplode', {
+    detail: eventData,
+    bubbles: true,
+  });
+  newBomb.dispatchEvent(explodeevent);
+};
+
+let moveBomb = function (newBomb, bombMovingStep, bombMovingInterval) {
+  let topPosition = 0 - newBomb.height;
+  newBomb.style.top = `${topPosition}px`;
+  newBomb.classList.remove('hidden');
+  let thownBombIntervalID = setInterval(function () {
+    newBomb.style.top = `${topPosition}px`;
+
+    topPosition += bombMovingStep;
+    if (topPosition > window.innerHeight) {
+      console.log('bomb will be removed');
+      clearInterval(thownBombIntervalID);
+      newBomb.remove();
+    }
+  }, bombMovingInterval);
+  console.log('throw bomb()');
+};
